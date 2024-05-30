eada928105c540244492e6429c72d8e339ff754a
kuba86952@wp.pl <kuba86952@wp.pl>
2024-05-31 01:48:40 +0200

    fixed problem with deploying on netlify

--- js/components/newTask.jsx
+++ js/components/newTask.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from './Button';
+import Button from './button';
 import { createTask } from '../api/tasks';
 
 function NewTask({ onNewTask }) {
