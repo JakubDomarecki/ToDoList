c066fc1df56ab2c3f12bc8dbc242abf899f8bf45
kuba86952@wp.pl <kuba86952@wp.pl>
2024-07-02 15:03:40 +0200

    fixed useEffect

--- js/components/Task.jsx
+++ js/components/Task.jsx
@@ -1,83 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import Operations from './Operations';
-import Button from './button.jsx';
-import { removeTask, updateTask } from '../api/tasks';
-import { getOperations } from '../api/operations';
+import React, { useState, useEffect } from "react";
+import Operations from "./Operations";
+import Button from "./button.jsx";
+import { removeTask, updateTask } from "../api/tasks";
+import { getOperations } from "../api/operations";
 
 function Task({ title, description, id, status: _status, onRemoveTask }) {
-    const [status, setStatus] = useState(_status);
-    const [operations, setOperations] = useState([]);
-    const [operationForm, setOperationForm] = useState(false);
+  const [status, setStatus] = useState(_status);
+  const [operations, setOperations] = useState([]);
+  const [operationForm, setOperationForm] = useState(false);
 
-    useEffect(() => {
-        getOperations(id, setOperations);
-    }, []);
+  useEffect(() => {
+    getOperations(id, setOperations);
+  }, [id]);
 
-    const toggleOperationForm = () => {
-        setOperationForm((prevState) => !prevState);
+  const toggleOperationForm = () => {
+    setOperationForm((prevState) => !prevState);
+  };
+
+  const handleFinish = () => {
+    const task = {
+      title,
+      description,
+      status: "closed",
     };
+    updateTask(id, task, () => {
+      setStatus("closed");
+    });
+  };
 
-    const handleFinish = () => {
-        const task = {
-            title,
-            description,
-            status: 'closed',
-        };
-        updateTask(id, task, () => {
-            setStatus('closed');
-        });
-    };
+  const handleRemove = () => {
+    removeTask(id, () => {
+      onRemoveTask(id);
+    });
+  };
 
-    const handleRemove = () => {
-        removeTask(id, () => {
-            onRemoveTask(id);
-        });
-    };
+  return (
+    <section className="card mt-5 shadow-sm">
+      <div className="card-header d-flex justify-content-between align-items-center">
+        <div>
+          <h5>{title}</h5>
+          <h6 className="card-subtitle text-muted">{description}</h6>
+        </div>
 
-    return (
-        <section className="card mt-5 shadow-sm">
-            <div className="card-header d-flex justify-content-between align-items-center">
-                <div>
-                    <h5>{title}</h5>
-                    <h6 className="card-subtitle text-muted">{description}</h6>
-                </div>
+        <div>
+          {status === "open" && (
+            <>
+              <Button
+                color="info"
+                small
+                onClick={toggleOperationForm}
+                className="mr-2"
+              >
+                Add operation
+              </Button>
 
-                <div>
-                    {status === 'open' && (
-                        <>
-                            <Button color="info" small onClick={toggleOperationForm} className="mr-2">
-                                Add operation
-                            </Button>
+              <Button color="dark" small onClick={handleFinish}>
+                Finish
+              </Button>
+            </>
+          )}
+          {operations.length === 0 && (
+            <Button
+              color={"danger"}
+              outline
+              small
+              onClick={handleRemove}
+              className="ml-2"
+            >
+              Delete
+            </Button>
+          )}
+        </div>
+      </div>
 
-                            <Button color="dark" small onClick={handleFinish}>
-                                Finish
-                            </Button>
-                        </>
-                    )}
-                    {operations.length === 0 && (
-                        <Button
-                            color={'danger'}
-                            outline
-                            small
-                            onClick={handleRemove}
-                            className="ml-2"
-                        >
-                            Delete
-                        </Button>
-                    )}
-                </div>
-            </div>
-
-            <Operations
-                taskID={id}
-                form={operationForm}
-                setForm={setOperationForm}
-                operations={operations}
-                setOperations={setOperations}
-                status={status}
-            />
-        </section>
-    );
+      <Operations
+        taskID={id}
+        form={operationForm}
+        setForm={setOperationForm}
+        operations={operations}
+        setOperations={setOperations}
+        status={status}
+      />
+    </section>
+  );
 }
 
 export default Task;
